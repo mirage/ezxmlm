d17230c99441a93195962116e6acef49dcb00048
Anil Madhavapeddy <anil@recoil.org>
2013-11-01 16:18:40 +0000

    add licenses

--- lib/ezxmlm.mli
+++ lib/ezxmlm.mli
@@ -1,3 +1,20 @@
+(*
+ * Copyright (c) 2013 Anil Madhavapeddy <anil@recoil.org>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ *)
+
 (** An "easy" interface on top of the [Xmlm] library.  This version provides
     more convenient (but far less flexible) input and output functions that
     go to and from [string] values.  This avoids the need to write signal code,
@@ -21,10 +38,10 @@ type nodes = node list
 val from_channel : in_channel -> Xmlm.dtd * nodes
 
 (** Read an XML document directly from a [string] *)
-val from_string  : string -> Xmlm.dtd * nodes
+val from_string : string -> Xmlm.dtd * nodes
 
 (** Low-level function to read directly from an [Xmlm] input source *)
-val from_input   : Xmlm.input -> Xmlm.dtd * node
+val from_input : Xmlm.input -> Xmlm.dtd * node
 
 (** {1 Writing XML values } *)
 
